d07f76a34a155957373e7d47b696acad6d034920
Rajnish Singh <kritrajnexer@gamil.com>
2026-07-23 12:25:33 +0530

    Slow scroll velocity, improve copy

--- components/home/ScrollVelocitySection.tsx
+++ components/home/ScrollVelocitySection.tsx
@@ -9,11 +9,11 @@ export default function ScrollVelocitySection() {
   return (
     <section className="relative overflow-hidden py-8 sm:py-12">
       <ScrollVelocityContainer className="font-bold text-4xl tracking-[-0.02em] text-ink/10 md:text-7xl md:leading-20">
-        <ScrollVelocityRow baseVelocity={15} direction={1}>
-          Websites That Sell &nbsp;•&nbsp; Automations That Scale &nbsp;•&nbsp; Results That Matter &nbsp;•&nbsp;
+        <ScrollVelocityRow baseVelocity={6} direction={1}>
+          Web Design &nbsp;•&nbsp; n8n Automation &nbsp;•&nbsp; CRM Integration &nbsp;•&nbsp; Lead Generation &nbsp;•&nbsp; SEO &nbsp;•&nbsp;
         </ScrollVelocityRow>
-        <ScrollVelocityRow baseVelocity={15} direction={-1}>
-          Design &nbsp;•&nbsp; Develop &nbsp;•&nbsp; Automate &nbsp;•&nbsp; Convert &nbsp;•&nbsp;
+        <ScrollVelocityRow baseVelocity={6} direction={-1}>
+          Strategy &nbsp;•&nbsp; Development &nbsp;•&nbsp; Automation &nbsp;•&nbsp; Growth &nbsp;•&nbsp;
         </ScrollVelocityRow>
       </ScrollVelocityContainer>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-bg" />
