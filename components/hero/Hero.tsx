e03d50dd6b144f7145bc35ad48049504b83a7c55
Rajnish Singh <kritrajnexer@gamil.com>
2026-07-23 12:17:34 +0530

    Remove FloatingPaths from Hero (was causing lag)

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -2,12 +2,10 @@ import Container from "@/components/Container";
 import HeroCopy from "./HeroCopy";
 import SystemInMotion from "./SystemInMotion";
 import ParallaxSection from "@/components/ParallaxSection";
-import { FloatingPaths } from "@/components/ui/BackgroundPaths";
 
 export default function Hero() {
   return (
     <section className="relative overflow-hidden py-20 sm:py-28">
-      <FloatingPaths position={1} />
       <Container className="relative z-10">
         <ParallaxSection speed={0.6}>
           <div className="grid grid-cols-1 items-center gap-12 lg:grid-cols-2 lg:gap-16">
